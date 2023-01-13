b4e11208f76eecd67c7335d43f0b99bb335a1f4c
jheon-eom <e4033jh@daum.net>
2023-01-13 20:18:30 +0900

    2023-01-13 commit

---
+++ src/main/java/inflearn/twopointersslidingwindow/최대_매출.java
@@ -0,0 +1,27 @@
+package inflearn.twopointersslidingwindow;
+
+import java.util.Scanner;
+
+public class 최대_매출 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int answer;
+        int sum = 0;
+        for (int i = 0; i < k; i++) sum += arr[i];
+        answer = sum;
+
+        for (int i = k; i < n; i++) {
+            sum += (arr[i] - arr[i - k]);
+            answer = Math.max(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+
+}
