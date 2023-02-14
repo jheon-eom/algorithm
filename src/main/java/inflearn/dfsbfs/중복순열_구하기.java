1b4c463443bdbfea0bc682ae1d7cf4127ffcf0d5
Jongheon Eom <e4033jh@daum.net>
2023-02-14 22:34:30 +0900

    2023-02-14 DFS

---
+++ src/main/java/inflearn/dfsbfs/중복순열_구하기.java
@@ -0,0 +1,32 @@
+package inflearn.dfsbfs;
+
+import java.util.Scanner;
+
+public class 중복순열_구하기 {
+    static int[] pm;
+    static int n;
+    static int m;
+
+    public static void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 1; i <= n; i++) {
+                pm[L] = i;
+                DFS(L + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        pm = new int[m];
+        DFS(0);
+    }
+}
+
