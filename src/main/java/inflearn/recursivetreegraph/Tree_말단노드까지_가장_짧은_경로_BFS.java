4dc220ce3f13ea0a5a7629cf97c4e8b5eea11c2e
jheon-eom <e4033jh@daum.net>
2023-02-21 08:54:10 +0900

    2023-02-21 조합수 구하기 DFS

--- src/main/java/inflearn/recursivetreegraph/Tree_말단노드까지_가장_짧은_경로_BFS.java
+++ src/main/java/inflearn/recursivetreegraph/Tree_말단노드까지_가장_짧은_경로_BFS.java
@@ -11,31 +11,31 @@ public class Tree_말단노드까지_가장_짧은_경로_BFS {
         root.rt = new Node(3);
         root.lt.lt = new Node(4);
         root.lt.rt = new Node(5);
-        System.out.println(BFS(root));
+//        System.out.println(BFS(root));
     }
 
-    public static int BFS(Node root) {
-        Queue<Node> Q = new LinkedList<>();
-        Q.offer(root);
-        int L = 0;
-        while (!Q.isEmpty()) {
-            for (int i = 0; i < Q.size(); i++) {
-                Node cur = Q.poll();
-                if (cur.lt == null && cur.rt == null) {
-                    return L;
-                } else {
-                    if (cur.lt != null) {
-                        Q.offer(cur.lt);
-                    }
-                    if (cur.rt != null) {
-                        Q.offer(cur.rt);
-                    }
-                }
-            }
-            L++;
-        }
-        return 0;
-    }
+//    public static int BFS(Node root) {
+//        Queue<Node> Q = new LinkedList<>();
+//        Q.offer(root);
+//        int L = 0;
+//        while (!Q.isEmpty()) {
+//            for (int i = 0; i < Q.size(); i++) {
+//                Node cur = Q.poll();
+//                if (cur.lt == null && cur.rt == null) {
+//                    return L;
+//                } else {
+//                    if (cur.lt != null) {
+//                        Q.offer(cur.lt);
+//                    }
+//                    if (cur.rt != null) {
+//                        Q.offer(cur.rt);
+//                    }
+//                }
+//            }
+//            L++;
+//        }
+//        return 0;
+//    }
 
     private static class Node {
 
